79ae3eced192c4cf4158fe3754fff03fdd9b9bc1
Francisco Arley Coelho Barros <contatofcarley@gmail.com>
2023-06-22 21:10:27 -0300

    Resolução

--- matrix.py
+++ matrix.py
@@ -1,44 +1,71 @@
 """Módulo com as funções de manipulação de matrizes."""
+from math import sqrt
 
 
 def soma(x: list[list[float]], y: list[list[float]]) -> list[list[float]] | None:
     """Soma duas matrizes"""
-    # TODO: implementar
-    # a soma de duas matrizes [[1, 2, 4], [2, 3, 4]] + [[2, 3, 4], [1, 2, 4]] é [[3, 5, 8], [3, 5, 8]]
-    # a soma só pode ser realizada se as matrizes tem a mesma quantidade de linhas e colunas.
-    # caso contrário, deve retornar None
+    if len(x) != len(y) or len(x[0]) != len(y[0]):
+        return None
+
+    result = []
+    for i in range(len(x)):
+        row = []
+        for j in range(len(x[0])):
+            row.append(x[i][j] + y[i][j])
+        result.append(row)
+
+    return result
 
 
 def multiplicação_por_escalar(matriz: list[list[float]], escalar: float) -> list[list[float]]:
     """Multiplica uma matriz por um escalar"""
-    # TODO: implementar
-    # a multiplicação de uma matriz [[1, 2, 4], [2, 3, 4]] por um escalar 2 é [[2, 4, 8], [4, 6, 8]]
+    result = []
+    for i in range(len(matriz)):
+        row = []
+        for j in range(len(matriz[i])):
+            row.append(matriz[i][j] * escalar)
+        result.append(row)
 
+    return result
 
 def multiplicação(x: list[list[float]], y: list[list[float]]) -> list[list[float]] | None:
     """Multiplica duas matrizes"""
-    # TODO: implementar
-    # a multiplicação de duas matrizes [[1, 2, 4], [2, 3, 4]] por [[2, 3, 4], [1, 2, 4]] é [[10, 17, 28], [12, 20, 32]]
-    # a multiplicação só pode ser realizada se a quantidade de colunas da primeira matriz é igual a quantidade de linhas da segunda matriz.
-    # caso contrário, deve retornar None
+    if len(x[0]) != len(y):
+        return None
 
+    result = []
+    for i in range(len(x)):
+        row = []
+        for j in range(len(y[0])):
+            element = 0
+            for k in range(len(y)):
+                element += x[i][k] * y[k][j]
+            row.append(element)
+        result.append(row)
+
+    return result
 
 def norma(x: list[list[float]]) -> float:
     """Calcula a norma de uma matriz"""
-    # TODO: implementar
-    # a norma de uma matriz [[1, 2, 4], [2, 3, 4]] é 6.928203230275509
-    # ela consiste em calcular a raiz quadrada da soma dos quadrados dos elementos da matriz
-    # caso a matriz esteja vazia deve-se retornar 0
+    if not x:
+        return 0
 
+    norm_squared = sum(sum(elem**2 for elem in row) for row in x)
+    norm = sqrt(norm_squared)
+
+    return norm
 
 def é_simétrica(x: list[list[float]]) -> bool:
     """Verifica se uma matriz é simétrica"""
-    # TODO: implementar
-    # uma matriz é simétrica se ela é quadrada e se ela é igual a sua transposta
-    # a transposta de uma matriz é a matriz que tem as linhas da matriz original como colunas e as colunas da matriz original como linhas
+    if len(x) != len(x[0]):
+        return False
 
+    transposed = transposta(x)
+
+    return x == transposed
 
 def transposta(x: list[list[float]]) -> list[list[float]]:
     """Calcula a transposta de uma matriz"""
-    # TODO: implementar
-    # a transposta de uma matriz [[1, 2, 4], [2, 3, 4]] é [[1, 2], [2, 3], [4, 4]]
+    transposed = [[x[j][i] for j in range(len(x))] for i in range(len(x[0]))]
+
+    return transposed
--- vector.py
+++ vector.py
@@ -1,50 +1,60 @@
 """Módulo com as funções de manipulação de vetores."""
-
+from typing import List, Union
+from math import sqrt
 
 def norma(x: list[float]) -> float | None:
     """Calcula a norma de um vetor"""
-    # TODO: implementar
-    # a norma de um vetor [3, 4] é 5
-    # a norma de um vetor [1, 2, 4] é 4.58257569495584
-    # ela consiste em calcular a raiz quadrada da soma dos quadrados dos elementos do vetor
-    # se o vetor estiver vazio retorne None
+    if not x:
+        return None
+
+    norm_squared = sum(elem**2 for elem in x)
+    norm = sqrt(norm_squared)
+
+    return norm
 
 
 def soma(x: list[float], y: list[float]) -> list[float] | None:
     """Soma dois vetores"""
-    # TODO: implementar
-    # a soma de dois vetores [1, 2, 4] + [2, 3, 4] é [3, 5, 8]
-    # a soma só pode ser realizada se os vetores tem a mesma quantidade de elementos.
-    # caso contrário, deve retornar None
+    if len(x) != len(y):
+        return None
+
+    result = [x[i] + y[i] for i in range(len(x))]
+    return result
 
 
 def multiplicação_por_escalar(vetor: list[float], escalar: float) -> list[float]:
     """Multiplica um vetor por um escalar"""
-    # TODO: implementar
-    # a multiplicação de um vetor [1, 2, 4] por um escalar 2 é [2, 4, 8]
+    result = [elem * escalar for elem in vetor]
+    return result
 
 
 def produto_interno(x: list[float], y: list[float]) -> float | None:
     """Calcula o produto interno de dois vetores"""
-    # TODO: implementar
-    # o produto interno de dois vetores [1, 2, 4] e [2, 3, 4] é 24
-    # o produto interno consiste em multiplicar os elementos de mesmo índice e somar os resultados
-    # a multiplicação só pode ser realizada se os vetores tem a mesma quantidade de elementos.
-    # caso contrário, deve retornar None
-    # caso os vetores sejam vazios o resultado é 0
+    if len(x) != len(y):
+        return None
+
+    result = sum(x[i] * y[i] for i in range(len(x)))
+    return result
 
 
 def produto_vetorial(x: list[float], y: list[float]) -> list[float] | None:
     """Calcula o produto vetorial de dois vetores"""
-    # TODO: implementar
-    # o produto vetorial de dois vetores [1, 2, 4] e [2, 3, 4] é [-4, 4, -1]
-    # o produto vetorial só pode ser realizado se os vetores tem 3 elementos.
-    # caso contrário, deve retornar None
+    if len(x) != 3 or len(y) != 3:
+        return None
+
+    result = [
+        x[1] * y[2] - x[2] * y[1],
+        x[2] * y[0] - x[0] * y[2],
+        x[0] * y[1] - x[1] * y[0]
+    ]
+    return result
+
 
 
 def produto_diádico(x: list[float], y: list[float]) -> list[list[float]] | None:
     """Calcula o produto diádico de dois vetores"""
-    # TODO: implementar
-    # o produto diádico de dois vetores [1, 2, 4] e [2, 3, 4] é [[2, 3, 4], [4, 6, 8], [8, 12, 16]]
-    # o produto diádico só pode ser realizado se os vetores tem a mesma quantidade de elementos.
-    # caso contrário, deve retornar None
+    if len(x) != len(y):
+        return None
+
+    result = [[x[i] * y[j] for j in range(len(y))] for i in range(len(x))]
+    return result
